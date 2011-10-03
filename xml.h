d2ed7db802da6fea14c29e1a8926dc2842c7ef5f
mrme <shinden@wp.pl>
2011-10-03 14:32:41 +0200

    xml test

---
+++ xml.h
@@ -0,0 +1,25 @@
+#ifndef XML_H
+#define XML_H
+
+//Q_IMPORT_PLUGIN(qmng)
+
+class E : public QLabel
+{
+  Q_OBJECT
+public:
+  E (QWidget *parent = 0){};
+/*
+  E (int, int, int, int, int, QString, QString, QWidget *parent = 0);
+  E (int, int, int, int, int, QWidget *parent = 0);
+  E (int, int, QString, QWidget *parent = 0);
+  E (int, int, int, int, int, int, QWidget *parent = 0);
+  enum {E_STOP,E_WORK,E_FAIL,E_FAIL2,E_PE,E_OFF,E_GREEN,E_RED,E_RED2,E_PP,E_PA};
+  void re(int);
+  int m,r,i;
+  QString p,a;
+  QMovie *X,*M;
+  QPalette green,red;
+*/
+};
+
+#endif
