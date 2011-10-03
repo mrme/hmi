fa2e888e6e6fe64e12cf6a9ddab3686230273279
mrme <shinden@wp.pl>
2011-10-03 14:38:28 +0200

    xml

--- xml.h
+++ xml.h
@@ -1,25 +1,10 @@
 #ifndef XML_H
 #define XML_H
 
-//Q_IMPORT_PLUGIN(qmng)
+#include <QtGui>
+#include <QDomDocument>
+#include <QFile>
 
-class E : public QLabel
-{
-  Q_OBJECT
-public:
-  E (QWidget *parent = 0){};
-/*
-  E (int, int, int, int, int, QString, QString, QWidget *parent = 0);
-  E (int, int, int, int, int, QWidget *parent = 0);
-  E (int, int, QString, QWidget *parent = 0);
-  E (int, int, int, int, int, int, QWidget *parent = 0);
-  enum {E_STOP,E_WORK,E_FAIL,E_FAIL2,E_PE,E_OFF,E_GREEN,E_RED,E_RED2,E_PP,E_PA};
-  void re(int);
-  int m,r,i;
-  QString p,a;
-  QMovie *X,*M;
-  QPalette green,red;
-*/
-};
+void  XML(QString);
 
 #endif
